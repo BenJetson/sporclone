fcf3ac1afdde15857e7689e98d161f9c6db59968
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-04-08 10:39:23 -0400

    Fixed loading sequence.

--- js/ajaxhandler.js
+++ js/ajaxhandler.js
@@ -1,14 +1,17 @@
-function ajaxGETRequest(file, id_location) {
+function loadGame(file) {
     
     var xhttp = new XMLHttpRequest();
     
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-            document.getElementById(id_location).innerHTML = xhttp.responseText;
+            document.getElementById(gameConfig).innerHTML = xhttp.responseText;
+            setup();
         }
     }
     
     xhttp.open("GET", file, true);
     xhttp.send();
+    
+    return False;
     
 }

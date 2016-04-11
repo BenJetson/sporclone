93c660bf8dee6c8089430f894ebc51df9d679abf
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-04-11 09:54:39 -0400

    Fixed some tags and more debug

--- js/ajaxhandler.js
+++ js/ajaxhandler.js
@@ -1,6 +1,8 @@
+var xhttp;
+
 function loadGame(file) {
     
-    var xhttp = new XMLHttpRequest();
+    xhttp = new XMLHttpRequest();
     
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {

1b4419fcd1036628449f5da64bb9d1b62eade52d
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-04-11 09:56:36 -0400

    Think AJAX should finally work.

--- js/ajaxhandler.js
+++ js/ajaxhandler.js
@@ -7,11 +7,8 @@ function loadGame(file) {
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             
-            console.log(xhttp.responseText);
-            
-            var serverResponse = xhttp.responseText;
-            var serverResponse = serverResponse.split("*");
-            var answerArray = serverResponse.split(0,-1);
+            var serverResponse = xhttp.responseText.split("*");
+            var answerArray = serverResponse.splice(0,-1);
             var timeAmt = Number(serverResponse[-1]);
             
             setup(answerArray, timeAmt);
